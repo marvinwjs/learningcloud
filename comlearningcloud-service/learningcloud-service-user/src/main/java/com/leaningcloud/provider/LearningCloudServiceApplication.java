5ff0f530ce000709bca9e741193c01837b0adf3b
wuyujian <wuyujian@ppdai.com>
2019-11-18 18:39:11 +0800

    新增服务提供模块

--- comlearningcloud-service/learningcloud-service-user/src/main/java/com/leaningcloud/provider/LearningCloudServiceApplication.java
+++ comlearningcloud-service/learningcloud-service-user/src/main/java/com/leaningcloud/provider/LearningCloudServiceApplication.java
@@ -1,20 +1,16 @@
-package com.learningcloud.gateway;
+package com.leaningcloud.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author Marvin
- * @date 2019-10-28 13:20
- * @version 1.0.0
  */
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZuulProxy
-public class LearningCloudGatewayApplication {
+public class LearningCloudServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(LearningCloudGatewayApplication.class, args);
+        SpringApplication.run(LearningCloudServiceApplication.class,args);
     }
 }
--- comlearningcloud-service/learningcloud-service-user/src/main/java/com/leaningcloud/provider/controller/admin/UserController.java
+++ comlearningcloud-service/learningcloud-service-user/src/main/java/com/leaningcloud/provider/controller/admin/UserController.java
@@ -1,24 +1,26 @@
-package com.learningcloud.gateway.controller;
+package com.leaningcloud.provider.controller.admin;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 配置接口
  * @author Marvin
- * @date 2019-11-01 13:40
+ * @date 2019-01-04 15:20
  * @version 1.0.0
+ * 用户行为Api
  */
 @RestController
-@RequestMapping(value = "config")
-public class ConfigController {
+@RequestMapping(value = "/userApi")
+public class UserController {
     @Value("${info.me}")
     private String info;
-
     @GetMapping("/info")
     public String getApiVersion() {
+        System.out.println(info);
         return info;
     }
+
 }
